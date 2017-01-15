447676bbe95230d5d2d000589a5344aa42df8a81
Chi <chiyc@live.com>
2017-01-15 10:11:23 -0700

    complete article creation form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def index
     @articles = Article.all
   end
@@ -7,5 +8,11 @@ class ArticlesController < ApplicationController
   end
   def new
     @article = Article.new
+  end
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
   end
 end
